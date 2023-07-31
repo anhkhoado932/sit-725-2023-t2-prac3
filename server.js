7a353d426581bc17c94b919a7347986074891e34
Do Anh Khoa <s223188874@deakin.edu.au>
2023-07-31 15:49:12 +1000

    practice with viewengine

--- server.js
+++ server.js
@@ -2,9 +2,30 @@ const express = require("express");
 var app = express();
 var port = process.env.port || 3000;
 
+const cardList = [
+  {
+    title: "Kitten 2",
+    path: "images/kitten.jpeg",
+    link: "About Kitten 2",
+    desciption: "Demo desciption about kitten 2",
+  },
+  {
+    title: "Kitten 3",
+    path: "images/kitten.jpeg",
+    link: "About Kitten 3",
+    desciption: "Demo desciption about kitten 3",
+  },
+];
+
 app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+  res.render("pages/index", { cats: cardList }); // view
+}); 
 
 app.listen(port, () => {
   console.log("App listening to: " + port);
